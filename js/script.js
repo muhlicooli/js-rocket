770be708cc50c187dcc36f41e1f6f4bc5e966b84
muhlicooli <simmymair@hotmail.de>
2021-02-07 17:21:28 +0100

    Made some car stuff happen

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var sayMyName = function (name) {
+	alert('My name is ' +name);
+}
+
+var car = {
+	make: 'VW',
+	type: 'Polo',
+	color:'blue',
+	isTurnedOn: false,
+	nrOfWheels: 4,
+	seats: [
+		'Vordersitz',
+		'Beifahrer',
+		'Rückbank'
+		],
+	turnOn: function() {
+		this.isTurnedOn = true;
+	},
+	fly: function() {
+		alert('Fly');
+	},
+	switchCar: function(isOn) {
+		console.log('Turn car ' + isOn)
+		if (isOn == true) {
+			this.isTurnedOn = true;
+		} else {
+			this.isTurnedOn = false;
+		}
+
+
+	} 
+
+}
---
+++ test.js
@@ -0,0 +1,42 @@
+var giggiObject = {
+	firstName: 'Michael',
+	lastName: 'Variola',
+	favPersons: ['muhli', 'simmy', 'mama maria'],
+	years: 29,
+	isPretty: true
+};
+
+var doSomething = function () {
+	console.log('Hallo Stinker');
+}
+
+var whatIsMyFavColor = function () {
+
+	return 'mustard';
+}
+var area = function (width, height) {
+return width * height;
+}
+if (true) {
+	alert ('Giggi ist süss');
+}
+if (1) {
+
+}
+var whatIsMyFavColor = function (color) {
+
+	if (color=='yellow') {
+		alert ('Hallo Gelbe Farbe');
+	}
+
+
+var name1='Simmy';
+var name2='Giggi';
+
+if (name1= 'Giggi') {
+	alert ('Hallo Giggi 1');
+} else if (name2=='Simmy') {
+	alert ('Hallo Simmy 2');
+} else {
+	alert ('Keiner da');
+}
