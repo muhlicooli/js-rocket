45dfe94a14dd634173286a6269215c9d95e71dbf
muhlicooli <simmymair@hotmail.de>
2021-02-08 21:49:17 +0100

    Made rocket fly

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+
+
+var doCoolStuff = function(){
+	var currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName=='cool'){
+		document.getElementById('cool').className = 'cool red';
+	}
+	else{
+		document.getElementById ('cool').className = 'cool';
+	} 
+}
+
 var sayMyName = function (name) {
 	alert('My name is ' +name);
 }
