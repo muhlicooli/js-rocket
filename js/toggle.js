1472e9bb6a536e24cf37ee82c1ef0ec6afb3cbfb
muhlicooli <simmymair@hotmail.de>
2021-03-28 16:17:51 +0200

    added night bg

--- js/toggle.js
+++ js/toggle.js
@@ -1,27 +1,29 @@
-class Vehicle {
-    constructor() {
-        
-    }
-}
-
 class Toggle {
     constructor() {
         this.state = false;
-        this.nightBg = 'linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 50%, rgba(0,212,255,1) 100%)';
+        this.nightBg = 'linear-gradient(180deg, rgba(15,10,113,1) 0%, rgba(76,36,193,1) 48%, rgba(23,184,195,1) 100%)';
         this.dayBg = 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)';
+        this.nightFontColor = '#4a23bf';
+        this.dayFontColor = '#87CEEB';
+        this.buttons = undefined;
     } 
     toggle() {
-        let right = document.getElementById('right');
-        let left = document.getElementById('left');
-        if(!this.state) {
-            right.style.background = this.nightBg;
-            left.style.background = this.nightBg;
+        this.buttons = document.getElementsByTagName('a');
 
+        if(!this.state) {
+            document.body.style.background = this.nightBg;
+            this.changeButtonColor(this.nightFontColor);
         } else {
-            right.style.background = this.dayBg;
-            left.style.background = this.dayBg; 
+            document.body.style.background = this.dayBg;
+            document.body.style.background = this.dayBg; 
+            this.changeButtonColor(this.dayFontColor);
         };
         this.state = !this.state;
+    }
+    changeButtonColor(color) {
+        for (let element of this.buttons) {
+            element.style.color = color;
+        }
     }
 }
 
