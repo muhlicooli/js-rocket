eae54326a77fbd8f9850c50069ee0ad8bbeecc05
muhlicooli <simmymair@hotmail.de>
2021-03-28 13:15:25 +0200

    changed toggle background

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,13 @@
 let toggle = function() {
-    let rocket = document.getElementById('rocket');
-    console.log(rocket);
+    let right = document.getElementById('right');
+    right.style.background = 'linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 50%, rgba(0,212,255,1) 100%)';
+    
+    let left = document.getElementById('left');
+    left.style.background = 'linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 50%, rgba(0,212,255,1) 100%)';
+    
+
+    
+
 }
+
 
